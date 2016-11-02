f3570f36548d0038d79e892890de5b7b61e9b6c6
Yihui Xie <xie@yihui.name>
2016-11-02 14:04:51 -0500

    functions to install Hugo and find hugo

---
+++ R/install.R
@@ -0,0 +1,129 @@
+#' Install Hugo
+#'
+#' Download the appropriate Hugo executable for your platform from Github and
+#' try to copy it to a system directory so \pkg{blogdown} can run the
+#' \command{hugo} command to build a site.
+#'
+#' This function tries to install Hugo to \code{Sys.getenv('APPDATA')} on
+#' Windows, \file{~/Library/Application Support} on macOS, and \file{~/bin/} on
+#' other platforms (such as Linux). If these directories are not writable, the
+#' package directory \file{Hugo} of \pkg{blogdown} will be used. If it still
+#' fails, you have to install Hugo by yourself and make sure it can be found via
+#' the environment variable \code{PATH}.
+#'
+#' This is just a helper function and may fail to choose the correct Hugo
+#' executable for your operating system, especially if you are not on Windows or
+#' Mac or a major Linux distribution. When in doubt, read the Hugo documentation
+#' and install it by yourself: \url{https://gohugo.io}.
+#' @param version The Hugo version number, e.g., \code{0.17}; the special value
+#'   \code{latest} means the latest version (fetched from Github releases).
+#' @param use_brew Whether to use Homebrew (\url{https://brew.sh}) on macOS to
+#'   install Hugo (recommended because it is much easier to manage packages).
+#'   Note Homebrew will be automatically installed if it has not been installed.
+#' @param force Whether to install Hugo even if it has already been installed.
+#'   This may be useful when upgrading Hugo (if you use Homebrew, run the
+#'   command \command{brew update && brew upgrade} instead).
+#' @export
+install_hugo = function(version = 'latest', use_brew = TRUE, force = FALSE) {
+
+  if (Sys.which('hugo') != '' && !force) return(invisible())
+
+  # in theory, should access the Github API using httr/jsonlite but this
+  # poor-man's version may work as well
+  if (version == 'latest') {
+    json = readLines(
+      'https://api.github.com/repos/spf13/hugo/releases/latest', warn = FALSE
+    )
+    r = '^.*?"tag_name":\\s*"([^"]+)",.*'
+    version = gsub(r, '\\1', grep(r, json, value = TRUE)[1])
+  }
+  version = gsub('^[vV]', '', version)  # pure version number
+  bit = if (grepl('64', Sys.info()[['machine']])) '64bit' else '32bit'
+  base = sprintf('https://github.com/spf13/hugo/releases/download/v%s/', version)
+  owd = setwd(tempdir())
+  on.exit(setwd(owd), add = TRUE)
+  unlink(sprintf('hugo_%s*', version), recursive = TRUE)
+
+  download_zip = function(OS, type = 'zip') {
+    zipfile = sprintf('hugo_%s_%s-%s.%s', version, OS, bit, type)
+    download2(paste0(base, zipfile), zipfile, mode = 'wb')
+    zipfile
+  }
+
+  if (is_windows()) {
+    files = utils::unzip(download_zip('Windows'))
+  } else if (is_osx()) {
+    if (use_brew) {
+      if (Sys.which('brew') == '') system2(
+        '/usr/bin/ruby',
+        '-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+      )
+      system2('brew', 'install hugo', stdout = FALSE)
+      return(invisible())
+    }
+    files = utils::unzip(download_zip('MacOS'))
+  } else {
+    # might be Linux; good luck
+    zipfile = download_zip('Linux', 'tar.gz')
+    files = utils::untar(zipfile, list = TRUE)
+    utils::untar(zipfile)
+  }
+  exec = files[grep(sprintf('^hugo_%s.+', version), basename(files))][1]
+  if (is_windows()) {
+    file.rename(exec, 'hugo.exe')
+    exec = 'hugo.exe'
+  } else {
+    file.rename(exec, 'hugo')
+    exec = 'hugo'
+    Sys.chmod(exec, '0755')  # chmod +x
+  }
+
+  success = FALSE
+  dirs = bin_paths()
+  for (destdir in dirs) {
+    dir.create(destdir, showWarnings = FALSE)
+    success = file.copy(exec, destdir, overwrite = TRUE)
+    if (success) break
+  }
+  file.remove(exec)
+  if (!success) stop(
+    'Unable to install Hugo to any of these dirs: ',
+    paste(dirs, collapse = ', ')
+  )
+  message('Hugo has been installed to ', normalizePath(destdir))
+}
+
+# possible locations of the Hugo executable
+bin_paths = function(dir = 'Hugo') {
+  if (is_windows()) {
+    path = Sys.getenv('APPDATA', '')
+    path = if (dir_exists(path)) file.path(path, dir)
+  } else if (is_osx()) {
+    path = '~/Library/Application Support'
+    path = if (dir_exists(path)) file.path(path, dir)
+  } else {
+    path = '~/bin'
+  }
+  path = c(path, system.file(dir, package = getPackageName()))
+  path
+}
+
+# find an executable from PATH, APPDATA, system.file(), ~/bin, etc
+find_exec = function(cmd, dir) {
+  path = Sys.which(cmd)
+  if (path != "") return(path)
+
+  for (d in bin_paths(dir)) {
+    exec = if (is_windows()) paste0(cmd, ".exe") else cmd
+    path = file.path(d, exec)
+    if (utils::file_test("-x", path)) break else path = ''
+  }
+
+  if (path == '') stop(
+    cmd, ' not found. If it is installed, please make sure the executable ',
+    'can be found via the PATH variable.'
+  )
+  path.expand(path)
+}
+
+find_hugo = function() find_exec('hugo', 'Hugo')
---
+++ R/utils.R
@@ -0,0 +1,38 @@
+pkg_file = function(...) {
+  system.file(..., package = getPackageName(), mustWork = TRUE)
+}
+
+# only copy files/dirs if they exist
+file.copy2 = function(from, to, ...) {
+  i = file.exists(from)
+  file.copy(from[i], if (length(to) == 1) to else to[i], ...)
+}
+
+is_windows = function() .Platform$OS.type == 'windows'
+is_osx = function() Sys.info()[['sysname']] == 'Darwin'
+is_linux = function() Sys.info()[['sysname']] == 'Linux'
+
+# adapted from webshot:::download_no_libcurl due to the fact that
+# download.file() cannot download Github release assets:
+# https://stat.ethz.ch/pipermail/r-devel/2016-June/072852.html
+download2 = function(url, ...) {
+  if (is_windows())
+    return(download.file(url, method = 'wininet', ...))
+
+  # if non-Windows, check for libcurl/curl/wget/lynx, call download.file with
+  # appropriate method
+  if (Sys.which('wget') != '') {
+    method = 'wget'
+  } else if (Sys.which('curl') != '') {
+    method = 'curl'
+    # curl needs to add a -L option to follow redirects
+    opts = options(download.file.extra = paste('-L', getOption('download.file.extra')))
+    on.exit(options(opts), add = TRUE)
+  } else if (Sys.which('lynx') != '') {
+    method = 'lynx'
+  } else {
+    stop('no download method found (wget/curl/lynx)')
+  }
+
+  download.file(url, method = method, ...)
+}
