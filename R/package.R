a7a9231f4312d68fe497dc6ed13516730129d54e
Yihui Xie <xie@yihui.name>
2016-11-04 17:16:37 -0500

    import some internal utility functions from bookdown and knitr

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' @import utils
+
+with_ext = bookdown:::with_ext
+readUTF8 = bookdown:::readUTF8
+writeUTF8 = bookdown:::writeUTF8
+dir_exists = bookdown:::dir_exists
+dir_create = bookdown:::dir_create
+
+attr = knitr:::attr
+in_dir = knitr:::in_dir
+`%n%` = knitr:::`%n%`
