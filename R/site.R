3944e85d53bb48679c2be7beee52cfb49d059688
Yihui Xie <xie@yihui.name>
2016-11-04 17:21:09 -0500

    a quick site function similar to bookdown::bookdown_site()

---
+++ R/site.R
@@ -0,0 +1,17 @@
+blogdown_site = function(input, ...) {
+
+  output_dir = publish_dir()
+  render = function(input_file, output_format, envir, quiet, encoding, ...) {
+    render_pages()
+    if (!quiet)
+      message("\nOutput created: ", paste0(output_dir, '/index.html'))
+  }
+
+  # return site generator
+  list(
+    name = basename(getwd()),
+    output_dir = output_dir,
+    render = render,
+    clean = function() {}
+  )
+}
