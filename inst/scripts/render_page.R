5f8a09dbb9227a793de2bb70a6f2da4d47786c82
Yihui Xie <xie@yihui.name>
2016-12-08 22:26:35 -0600

    do not add version numbers to dep dir names

--- inst/scripts/render_page.R
+++ inst/scripts/render_page.R
@@ -1,4 +1,5 @@
 local({
+  options(htmltools.dir.version = FALSE)
   args = commandArgs(TRUE)
   rmarkdown::render(
     args[1], 'blogdown::html_page', envir = globalenv(), quiet = TRUE, encoding = 'UTF-8'
