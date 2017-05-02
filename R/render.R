523b892cf25c14d4208284ec30a260b5e7f54abc
Yihui Xie <xie@yihui.name>
2017-05-02 14:35:09 -0500

    fc7b2bcd was problematic: when raw = TRUE, we should not remove the content between <!-- BLOGDOWN-HEAD --> and <!-- /BLOGDOWN-HEAD -->, otherwise HTML widgets libraries will be lost

--- R/render.R
+++ R/render.R
@@ -99,7 +99,7 @@ build_rmds = function(files, config, local, raw = FALSE) {
     x = readUTF8(html)
     x = encode_paths(x, by_products(f, '_files'), d, raw, root)
     if (getOption('blogdown.widgetsID', TRUE)) x = clean_widget_html(x)
-    if (raw) x = split_html_tokens(x)$body
+    if (raw) x = split_html_tokens(x, FALSE)$body
     writeUTF8(c(fetch_yaml2(f), '', x), html)
   })
 
