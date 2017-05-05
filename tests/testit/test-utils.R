721d4223c7228f995e6b9bbec7816ee9deefde58
Yihui Xie <xie@yihui.name>
2017-05-05 12:27:13 -0500

    the RcppTOML::parseTOML() list has some attributes, so a helper function raw_list() to drop them, except the names

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -5,10 +5,20 @@ assert(
   dir_exists(pkg_file('resources'))
 )
 
+raw_list = function(x) {
+  if (!is.list(x)) return(x)
+  nms = names(x)
+  attributes(x) = NULL
+  names(x) = nms
+  x
+}
+
+x1 = c('a = "foo"', '# a comment', 'b = false', 'c3 = 10')
+x2 = list(a = 'foo', b = FALSE, c3 = 10L)
 assert(
   'parse_toml() works',
-  parse_toml(x = c('a = "foo"', '# a comment', 'b = false', 'c3 = 10')) %==%
-    list(a = 'foo', b = FALSE, c3 = 10)
+  raw_list(parse_toml(x = x1)) %==% x2,
+  parse_toml(x = x1, strict = FALSE) %==% x2
 )
 
 assert(
