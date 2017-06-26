6f702955e86eff60b8f2eb5ede18fba6b57f1e8b
Yihui Xie <xie@yihui.name>
2017-06-26 14:10:59 -0500

    fix #117: library(methods) to avoid confusion

--- inst/scripts/render_page.R
+++ inst/scripts/render_page.R
@@ -1,5 +1,6 @@
 local({
   options(htmltools.dir.version = FALSE)
+  library(methods)
   args = commandArgs(TRUE)
   rmarkdown::render(
     args[1], 'blogdown::html_page', envir = globalenv(), quiet = TRUE, encoding = 'UTF-8'
