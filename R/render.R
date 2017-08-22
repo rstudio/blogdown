2445fdf83dec9dfd231e6402b9593d3e1dd48f0b
Yihui Xie <xie@yihui.name>
2017-08-21 22:02:15 -0500

    reorganize the code to return early when method = 'custom', and run R/build.R _before_ hugo_build()

--- R/render.R
+++ R/render.R
@@ -34,14 +34,14 @@ build_site = function(
 ) {
   if (missing(method)) method = getOption('blogdown.method', method)
   method = match.arg(method)
-  if (method == 'html') {
-    files = list_rmds('content', TRUE)
-    if (local && length(files)) {
-      files = files[mapply(require_rebuild, output_file(files), files)]
-    }
-    build_rmds(files, load_config(), local, run_hugo)
+  on.exit(run_script('R/build.R', as.character(local)), add = TRUE)
+  if (method == 'custom') return()
+  files = list_rmds('content', TRUE)
+  if (local && length(files)) {
+    files = files[mapply(require_rebuild, output_file(files), files)]
   }
-  run_script('R/build.R', as.character(local))
+  build_rmds(files)
+  if (run_hugo) on.exit(hugo_build(local), add = TRUE)
   invisible()
 }
 
