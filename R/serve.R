74a6fb71f804e3be1bdb4eb8c622b0c471214ac4
Yihui Xie <xie@yihui.name>
2017-11-06 16:55:41 -0600

    use servr v0.8

--- R/serve.R
+++ R/serve.R
@@ -75,7 +75,6 @@ serve_it = function(
     g = generator()
     cmd = if (g == 'hugo') find_hugo() else g
     host = server$host; port = server$port; intv = server$interval
-    if (is.null(server$url)) server$url = sprintf('http://%s:%d', host, port)
     args_fun = match.fun(paste0(g, '_server_args'))
     cmd_args = args_fun(host, port)
     p1 = proc_new(cmd, cmd_args)
