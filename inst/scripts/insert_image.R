7b8761b07867e6a6a598452c9783d3e6a11f27d9
Leonardo Collado Torres <lcollado@jhsph.edu>
2018-03-06 18:38:58 -0500

    Address second round of requested changes

--- inst/scripts/insert_image.R
+++ inst/scripts/insert_image.R
@@ -6,16 +6,15 @@ local({
 
   blogpath = normalizePath(blogfile$path)
   imgdir = file.path(
-    gsub('/content/.*', '', blogpath),
     'static',
     dirname(gsub('.*content/', '', blogpath)),
     paste0(xfun::sans_ext(basename(blogpath)), '_files')
   )
-
   shiny::runGadget(
     miniUI::miniPage(miniUI::miniContentPanel(
       shiny::fillRow(
         shiny::fileInput('newimg', 'Image', placeholder = 'Select external image'),
+        shiny::column(width = 6, offset = 2, shiny::uiOutput('overbutton')),
         height = '90px'
       ),
       shiny::fillRow(
@@ -27,48 +26,54 @@ local({
         height = '70px'
       ),
       shiny::fillRow(
-        txt_input('imgdir', 'Target image location (customize only if necessary)', imgdir),
+        txt_input('imgfinal', 'Target image file', '', '(optional) customize only if necessary. Autotomatically fills after choosing an image.'),
         height = '70px'
-      ),
-      shiny::fillRow(
-        shiny::radioButtons(
-          'imgoverwrite', 'Overwrite image?', inline = TRUE,
-          c('Yes' = TRUE, 'No' = FALSE), selected = FALSE),
-          height = '70px'
       ),
       miniUI::gadgetTitleBar(NULL)
     )),
     server = function(input, output, session) {
+      shiny::observeEvent(input$newimg, {
+        shiny::updateTextInput(session, 'imgfinal', value = file.path(imgdir,
+          input$newimg$name))
+      })
+      shiny::observeEvent(input$imgfinal, {
+        if(file.exists(input$imgfinal)) {
+          output$overbutton = shiny::renderUI({
+            shiny::radioButtons(
+              'imgoverwrite', 'Target image exists. Overwrite it?',
+              inline = TRUE, c('Yes' = TRUE, 'No' = FALSE), selected = FALSE)
+          })
+        } else {
+          ## Reset in case the user changes the target image name and
+          ## the new target image doesn't exist
+          output$overbutton = shiny::renderUI({})
+        }
+      })
       shiny::observeEvent(input$done, {
         if (is.null(input$newimg)) return(
           warning('You have to choose an image!', call. = FALSE)
         )
-        imgfinaldir <- ifelse(input$imgdir == '', imgdir, input$imgdir)
-        if(!as.logical(input$imgoverwrite)) {
-            if(file.exists(file.path(imgfinaldir, input$newimg$name))) {
-                warning('The image already exists and you chose not to overwrite it!', call. = FALSE)
-            }
+        if(file.exists(input$imgfinal)) {
+          if(!as.logical(input$imgoverwrite)) {
+            warning('The image already exists and you chose not to overwrite it! Linking to the previous version of the image.', call. = FALSE)
+          }
         }
+        imgfinaldir = dirname(input$imgfinal)
         dir.create(imgfinaldir, showWarnings = FALSE, recursive = TRUE)
-        copy_check = file.copy(input$newimg$datapath,
-            file.path(imgfinaldir, input$newimg$name),
-            overwrite = as.logical(input$imgoverwrite))
-        if(copy_check) message(paste('successfully copied the image to', file.path(imgfinaldir, input$newimg$name)))
+        copy_check = file.copy(input$newimg$datapath, input$imgfinal,
+          overwrite = ifelse(is.null(input$imgoverwrite), FALSE,
+          as.logical(input$imgoverwrite)))
+        if(copy_check) message(paste('successfully copied the image to', input$imgfinal))
         
         imgsrc = paste0("/", basename(dirname(imgfinaldir)), "/",
-            basename(imgfinaldir), "/", input$newimg$name)
+          basename(imgfinaldir), "/", basename(input$imgfinal))
                 
         image_text = if(input$imgwidth == '' & input$imgheight == '') {
-            paste0('![', input$imgalt, '](', imgsrc, ')')
-        } else if (input$imgwidth == '') {
-            shiny::img(src = imgsrc, alt = input$imgalt, 
-                height = input$imgheight)
-        } else if (input$imgheight == '') {
-            shiny::img(src = imgsrc, alt = input$imgalt, 
-                width = input$imgwidth) 
+          paste0('![', input$imgalt, '](', imgsrc, ')')
         } else {
-            shiny::img(src = imgsrc, alt = input$imgalt,
-                width = input$imgwidth, height = input$imgheight)
+          shiny::img(src = imgsrc, alt = input$imgalt,
+            width = ifelse(input$imgwidth == '', NULL, input$imgwidth),
+            height = ifelse(input$imgheight == '', NULL, input$imgheight))
         }
         
         rstudioapi::insertText(as.character(image_text))
@@ -78,6 +83,6 @@ local({
         shiny::stopApp()
       })
     },
-    stopOnCancel = FALSE, viewer = shiny::dialogViewer('Add external image to a blogdown post', height = 450)
+    stopOnCancel = FALSE, viewer = shiny::dialogViewer('Add external image to a blogdown post', height = 380)
   )
 })
