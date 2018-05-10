d1934655ce0f4ac45ae5eb0dbc7e1d58e7480ba5
Yihui Xie <xie@yihui.name>
2018-05-10 10:24:28 -0500

    close #294: add a new RStudio addin "Touch File", which uses `touch` to update the mtime of a file if available, otherwise modify the file, undo it, and save it to update the mtime

--- R/utils.R
+++ R/utils.R
@@ -616,3 +616,26 @@ args_string = function(...) {
     paste(m, '=', v, sep = '', collapse = ' ')
   }
 }
+
+# use touch to update the timestamp of a file if available (not on Windows);
+# otherwise modify a file, undo it, and save it
+touch_file = function() {
+  ctx = rstudioapi::getSourceEditorContext()
+  if (!file.exists(ctx$path)) stop('The current document has not been saved yet.')
+  p = normalizePath(ctx$path); mtime = function() file.info(p)[, 'mtime']
+  m = mtime(); check_mtime = function() {
+    if (u <- !identical(m, m2 <- mtime())) message(
+      'The modification time of "', p, '" has been updated from ', m, ' to ', m2
+    )
+    u
+  }
+  if (Sys.which('touch') != '') {
+    if (system2('touch', shQuote(p)) == 0 && check_mtime()) return(TRUE)
+  }
+  id = ctx$id
+  # add a space and delete it, then (re)save the document to update its mtime
+  rstudioapi::insertText(c(1, 1), ' ', id = id)
+  rstudioapi::modifyRange(list(c(1, 1, 1, 2)), '', id)
+  rstudioapi::documentSave(id)
+  check_mtime()
+}
