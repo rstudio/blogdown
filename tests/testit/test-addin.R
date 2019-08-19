7a8897be6cbecda981ab27ecc4d352f211c84707
novica <nnovica@gmail.com>
2019-08-19 23:29:54 +0200

    remove last line test

--- tests/testit/test-addin.R
+++ tests/testit/test-addin.R
@@ -14,10 +14,4 @@ assert('quote_poem() add > to the beginning of every paragraph, and two trailing
   (quote_poem("some text.    \nsome more text.     \n\neven more text.")) %==%
     "> some text.  \nsome more text.\n\n> even more text."
 
-  # Note: quote_poem() doesn't add two spaces to the last line
-  # of each paragraph. I am not sure if this is by design.
-  # The following will not pass the test.
-  (quote_poem("some text.    \nsome more text.     \neven more text.")) %==%
-    "> some text.  \nsome more text.  \neven more text.  "
-
 })
