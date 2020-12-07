b59ea0283edf53738599a70a27401b85c4d0dafa
Alison Hill <apreshill@gmail.com>
2020-12-07 07:19:06 -0500

    new_site reverts back to lithium

--- R/hugo.R
+++ R/hugo.R
@@ -133,7 +133,7 @@ change_config = function(name, value) {
 #' if (interactive()) blogdown::new_site()
 new_site = function(
   dir = '.', install_hugo = TRUE, format = 'yaml', sample = TRUE,
-  theme = 'yihui/hugo-prose', hostname = 'github.com', theme_example = TRUE,
+  theme = 'yihui/hugo-lithium', hostname = 'github.com', theme_example = TRUE,
   empty_dirs = FALSE, to_yaml = TRUE, netlify = TRUE, .Rprofile = TRUE,
   serve = interactive()
 ) {
