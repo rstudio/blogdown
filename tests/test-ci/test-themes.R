1f2d1b9d0713bfae7d1d086758a4776962587e44
Yihui Xie <xie@yihui.name>
2022-09-16 09:34:47 -0500

    normalize paths before comparison

--- tests/test-ci/test-themes.R
+++ tests/test-ci/test-themes.R
@@ -1,6 +1,12 @@
 library(testit)
 
+test_site = function(theme) {
+  dir.create(d1 <- tempfile())
+  on.exit(unlink(d1, recursive = TRUE), add = TRUE)
+  d2 = new_site(d1, theme = theme)
+  xfun::normalize_path(d1) %==% xfun::normalize_path(d2)
+}
+
 assert('new_site() works', {
-  dir.create(d <- tempfile())
-  (new_site(d, theme = 'yihui/hugo-xmin') %==% d)
+  (test_site('yihui/hugo-xmin'))
 })
