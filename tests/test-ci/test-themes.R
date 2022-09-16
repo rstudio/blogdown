d4ae6849998afc5c1ae61698d60255bfc647b8ca
Yihui Xie <xie@yihui.name>
2022-09-16 09:48:03 -0500

    test more themes (#707)

--- tests/test-ci/test-themes.R
+++ tests/test-ci/test-themes.R
@@ -4,9 +4,15 @@ test_site = function(theme) {
   dir.create(d1 <- tempfile())
   on.exit(unlink(d1, recursive = TRUE), add = TRUE)
   d2 = new_site(d1, theme = theme)
+  xfun::in_dir(d2, blogdown::build_site(build_rmd = 'newfile'))
   xfun::normalize_path(d1) %==% xfun::normalize_path(d2)
 }
 
-assert('new_site() works', {
-  (test_site('yihui/hugo-xmin'))
+assert('new_site() and build_site() work with selected themes', {
+  themes = c(
+    'hugo-apero/hugo-apero',
+    sprintf('wowchemy/starter-hugo-%s', c('academic', 'online-course', 'research-group')),
+    sprintf('yihui/hugo-%s', c('lithium', 'prose', 'xmag', 'xmin'))
+  )
+  (sapply(themes, test_site))
 })
