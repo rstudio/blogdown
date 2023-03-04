54155287a6e09a4649aba33f979f31361c9838a8
Yihui Xie <xie@yihui.name>
2023-03-03 20:04:25 -0600

    tweak tests for quote_poem() accordingly

--- tests/test-cran/test-addin.R
+++ tests/test-cran/test-addin.R
@@ -1,16 +1,16 @@
-assert('quote_poem() adds > to the beginning of every paragraph, and two trailing spaces to every line', {
+assert('quote_poem() adds > to the beginning of every line, and two trailing spaces to every line', {
 
   (quote_poem("               ")  %==% "               ")
 
   (quote_poem("some text.") %==% '> some text.')
 
   (quote_poem("some text.\nsome more text.\neven more text.")) %==%
-    "> some text.  \nsome more text.  \neven more text."
+    "> some text.  \n> some more text.  \n> even more text."
 
   (quote_poem("some text.    \nsome more text.     \neven more text.")) %==%
-    "> some text.  \nsome more text.  \neven more text."
+    "> some text.  \n> some more text.  \n> even more text."
 
   (quote_poem("some text.    \nsome more text.     \n\neven more text.")) %==%
-    "> some text.  \nsome more text.\n\n> even more text."
+    "> some text.  \n> some more text.\n>\n> even more text."
 
 })
